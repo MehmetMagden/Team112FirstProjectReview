ac53088031decc93257733993aeb5a9f739f8ee8
Ibrahim Unal <125129575+iunal@users.noreply.github.com>
2023-06-01 20:36:52 +0200

    US_013 is completed

--- src/test/java/pages/HomeBase.java
+++ src/test/java/pages/HomeBase.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -7,7 +9,14 @@ public class HomeBase {
 
     public HomeBase(){
         PageFactory.initElements(Driver.getDriver(),this);
-
     }
+
+    @FindBy (xpath = "//button[text()='ACCEPT']")
+    public WebElement acceptCookiesButton;
+
+    @FindBy (xpath = "(//a[text()='Destinations'])[2]")
+    public WebElement homePageDestinationsButton;
+
+
 
 }
---
+++ src/test/java/tests/US_013.java
@@ -0,0 +1,49 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.DestinationsPage;
+import pages.HomeBase;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.TestBaseRapor;
+
+public class US_013 extends TestBaseRapor {
+
+    HomeBase homeBase = new HomeBase();
+    DestinationsPage destinationsPage = new DestinationsPage();
+
+    @Test
+    public void TC_13_01() {
+
+        //1) User opens browser
+        //2) User navigates the main page
+        Driver.getDriver().get(ConfigReader.getProperty("tripAndWayUrl"));
+
+        //3) User clicks the accept cookies button
+        homeBase.acceptCookiesButton.click();
+
+        //4) Url Should be same with the test data
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+        String expectedUrl = ConfigReader.getProperty("tripAndWayUrl");
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+        //5)User should see "Destination" button in the header section
+        Assert.assertTrue(homeBase.homePageDestinationsButton.isEnabled());
+
+        //6)User clicks the "Destination" button
+        homeBase.homePageDestinationsButton.click();
+
+        //7) User should see "Destinations" page
+        actualUrl = Driver.getDriver().getCurrentUrl();
+        expectedUrl = ConfigReader.getProperty("destinationsUrl");
+
+        //8) Url Should be same with the test data
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+        //9) User should see "Destinations" header
+        Assert.assertTrue(destinationsPage.destinationsImageWebElement.isDisplayed());
+
+        //10) User closes the browser
+    }
+}
