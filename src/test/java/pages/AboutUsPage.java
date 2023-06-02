8552e4840ca939340719910625eee94359794985
Sedaylcn <syalcin1912@hotmail.com>
2023-06-02 01:35:16 +0100

    About Us

--- src/test/java/pages/AboutUsPage.java
+++ src/test/java/pages/AboutUsPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -7,8 +9,22 @@ public class AboutUsPage {
 
     public AboutUsPage(){
         PageFactory.initElements(Driver.getDriver(),this);
-
-
     }
+
+    @FindBy (xpath = "(//a[normalize-space()='About Us'])[2]")
+    public WebElement aboutUsHeaderLink;
+
+
+    @FindBy (xpath = "//button[@type='button']")
+    public WebElement acceptCookies;
+
+    @FindBy (xpath = "//h3[normalize-space()='OUR MISSION']")
+    public WebElement aboutUsPageOurMission;
+
+    @FindBy (xpath = "//h3[normalize-space()='OUR VISION']")
+    public WebElement AboutUsPageOurVision;
+
+    @FindBy (xpath = "//div[@class='banner-text']")
+    public WebElement aboutUsBanner;
 
 }
---
+++ src/test/java/tests/US15.java
@@ -0,0 +1,48 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AboutUsPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class US15 {
+
+    AboutUsPage aboutUsPage = new AboutUsPage();
+
+    @Test
+    public void aboutUsPageTest (){
+
+     //1- User opens browser and go to the webpage
+        Driver.getDriver().get(ConfigReader.getProperty("tripAndWayURL"));
+
+    // 2- User go to the About Us Page
+        aboutUsPage.aboutUsHeaderLink.click();
+
+    //3 -Accept cookies
+         aboutUsPage.acceptCookies.click();
+
+    // 4- Check if "OUR MISSION" is visiable
+
+    String expectedWordMission= "OUR MISSION";
+    String actualWordMission = aboutUsPage.aboutUsPageOurMission.getText();
+
+    Assert.assertTrue(aboutUsPage.aboutUsPageOurMission.isDisplayed());
+
+   // 5- Check if "OUR VISION" is visiable
+
+    String expectedWordVision = "OUR VISION";
+    String actualWordVision = aboutUsPage.AboutUsPageOurVision.getText();
+
+    Assert.assertTrue(aboutUsPage.AboutUsPageOurVision.isDisplayed());
+
+    // 6 - Check if "About Us" is NOT appeared in the banner
+
+    String expectedWordAboutUs = "About Us";
+    String actualWordAboutUs = aboutUsPage.aboutUsBanner.getText();
+
+    Assert.assertTrue(!actualWordAboutUs.contains(expectedWordAboutUs));
+
+    }
+
+}
