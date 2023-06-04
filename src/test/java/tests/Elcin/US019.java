6b0a1bc9b176e27eadef889474c83d6e827f7cc4
ElcinCebeciKinsiz <128594772+ElcinCebeciKinsiz@users.noreply.github.com>
2023-06-04 17:23:06 +0100

    04.06.23 Elcin US019

--- src/test/java/tests/Elcin/US019.java
+++ src/test/java/tests/Elcin/US019.java
@@ -14,26 +14,26 @@ public class US019 {
 
 LoginPage loginPage=new LoginPage();
   @Test
-  public void TC019_01logInToWebPageWithRegisteredNameAndPassword(){
-  LoginPage loginPage=new LoginPage();
-  BasePage basePage=new BasePage();
+  public void TC019_01logInToWebPageWithRegisteredNameAndPassword() {
+    LoginPage loginPage = new LoginPage();
+    BasePage basePage = new BasePage();
 
-  Driver.getDriver().get(ConfigReader.getProperty("tripAndWayUrl"));
-  basePage.acceptCookiesButton.click();
-  ReusableMethods.waitFor(2);
-  loginPage.loginButtonHomePage.click();
-  loginPage.loginPageEmailAddressTextBox.sendKeys(ConfigReader.getProperty("userLoginEmailCorrect"));
-  loginPage.loginPagePasswordBox.sendKeys(ConfigReader.getProperty("userLoginPasswordCorrect"));
-  loginPage.loginPageLoginButton.click();
-  ReusableMethods.waitFor(2);
-  basePage.homePageHomeButton.click();
-  String actualUrl=Driver.getDriver().getCurrentUrl();
-  String expectedUrl="https://qa.tripandway.com/";
-  Assert.assertEquals(actualUrl,expectedUrl);
-  Driver.quitDriver();
+    Driver.getDriver().get(ConfigReader.getProperty("tripAndWayUrl"));
+    basePage.acceptCookiesButton.click();
+    ReusableMethods.waitFor(2);
+    loginPage.loginButtonHomePage.click();
+    loginPage.loginPageEmailAddressTextBox.sendKeys(ConfigReader.getProperty("userLoginEmailCorrect"));
+    loginPage.loginPagePasswordBox.sendKeys(ConfigReader.getProperty("userLoginPasswordCorrect"));
+    loginPage.loginPageLoginButton.click();
+    ReusableMethods.waitFor(2);
+    basePage.homePageHomeButton.click();
+    String actualUrl = Driver.getDriver().getCurrentUrl();
+    String expectedUrl = "https://qa.tripandway.com/";
+    Assert.assertEquals(actualUrl, expectedUrl);
+    Driver.quitDriver();
 
 
-}
+  }
 
 
 }
