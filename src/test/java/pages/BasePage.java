b3286862426a31106073d11f5d77dc159269d137
ElcinCebeciKinsiz <128594772+ElcinCebeciKinsiz@users.noreply.github.com>
2023-06-05 01:38:40 +0100

    05.06.23 Elcin-US006

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -74,10 +74,36 @@ public class BasePage {
     @FindBy (xpath = "//a[normalize-space()='7 days in Istanbul']")
     public WebElement sevenDaysInIstanbulButtonInLatestPackagesInFooter;
 
+    @FindBy(xpath = "(//div[@data-wow-delay='0.4s'])[2]")
+    public WebElement footerRecentPosts;
+
+    @FindBy (xpath = "(//a)[154]")
+    public WebElement recentPostsFirstLink;
+
+
+    @FindBy(xpath = "(//a)[155]")
+    public WebElement recentPostsSecondLink;
 
 
 
+    @FindBy(xpath = "(//a)[156]")
+    public WebElement recentPostsThirdLink;
 
+    @FindBy(xpath = "(//a)[157]")
+    public WebElement recentPostsFourthLink;
+
+
+    @FindBy(xpath = "(//a)[158]")
+    public WebElement recentPostsFifthLink;
+
+    @FindBy (xpath = "//h3[text()='Comments']")
+    public WebElement recentPostsLinkExpectedWord;
+
+    @FindBy(xpath = "(//h3)[5]")
+    public WebElement recentPostsSecondAndThirdExpectedWord;
+
+    @FindBy(xpath ="(//h3)[3]")
+    public WebElement getRecentPostsFourthAndFifthLinkExpectedWord;
 
 
 }
