26eeada1141e3125effad54d0b4a26497c292e2c
Kaan Yalcin <spoody@hotmail.co.uk>
2023-06-08 19:57:48 +0100

    Kaans commit

--- src/test/java/tests/Kaan/US_030.java
+++ src/test/java/tests/Kaan/US_030.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
-
 import org.testng.annotations.Test;
 import pages.AdminPage;
 import utilities.ConfigReader;
