42886c6026dd8dc7e57f59a729b0eefd9e2a3147
Mohammad Habibi <shabirhabibi426@gmail.com>
2023-06-10 18:09:20 +0100

    mohammad

--- src/test/java/tests/Muhammed/US_039.java
+++ src/test/java/tests/Muhammed/US_039.java
@@ -1,8 +1,11 @@
 package tests.Muhammed;
 
 import com.beust.ah.A;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.AdminPage;
 import pages.BasePage;
@@ -19,24 +22,54 @@ public class US_039 extends TestBaseRapor {
     Actions actions = new Actions(Driver.getDriver());
 
 
-    @Test
-    public void US039_TC01_AdminPageProfileEdition(){
 
+    @BeforeMethod
+    public void setUp() {
         Driver.getDriver().get(ConfigReader.getProperty("tripAndWayAdminURL"));
+
+    }
+    @AfterMethod
+    public void tearDown() {Driver.closeDriver();
+
+    }
+
+
+
+    @Test
+    public void US039_TC01_AdminPagelogin() {
+
+        AdminPage adminPage = new AdminPage();
+        Actions actions = new Actions(Driver.getDriver());
+
+        // Driver.getDriver().get(ConfigReader.getProperty("tripAndWayAdminURL"));
         adminPage.adminLoginEmailAdressTextBox.sendKeys("admin01@tripandway.com");
         adminPage.adminLogInPasswordTextBox.sendKeys("123123123");
         adminPage.adminLogInButton.submit();
         ReusableMethods.waitFor(2);
 
-        adminPage.AdminMainPageProfile.click();
+    }
+    @Test
+    public void US039_TC02_AdminPageprofileChang() {
+
+        AdminPage adminPage = new AdminPage();
+        Actions actions = new Actions(Driver.getDriver());
+
+
+        adminPage.adminLoginEmailAdressTextBox.sendKeys("admin01@tripandway.com");
+        adminPage.adminLogInPasswordTextBox.sendKeys("123123123");
+        adminPage.adminLogInButton.submit();
         ReusableMethods.waitFor(2);
+
+
+        adminPage.AdminMainPageProfile.click();
         String ExpectingChangePhoto = "Change Photo";
         String ActualChangingPhoto = adminPage.AdminChangingIMG.getText();
-        Assert.assertEquals(ActualChangingPhoto,ExpectingChangePhoto);
+        Assert.assertEquals(ActualChangingPhoto, ExpectingChangePhoto);
         adminPage.AdminChangingIMG.click();
         ReusableMethods.waitFor(2);
         //There is a bug on Update button
         Assert.assertTrue(adminPage.AdminPhotoUpdate.isEnabled());
+        ReusableMethods.waitFor(5);
 
 
     }
