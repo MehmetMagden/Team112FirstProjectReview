91e2f1ea2c475f4cc31ed94d2d0b8f15025ea2e3
ElcinCebeciKinsiz <128594772+ElcinCebeciKinsiz@users.noreply.github.com>
2023-06-10 12:20:39 +0100

    10.06.23 Elcin

--- src/test/java/tests/Elcin/US019.java
+++ src/test/java/tests/Elcin/US019.java
@@ -15,38 +15,39 @@ import utilities.TestBaseRapor;
 import java.awt.dnd.DragGestureEvent;
 
 public class US019 extends TestBaseRapor {
-  @BeforeMethod
-  public void setUp() {
-    Driver.getDriver().get(ConfigReader.getProperty("tripAndWayUrl"));
-    JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+    @BeforeMethod
+    public void setUp() {
+        Driver.getDriver().get(ConfigReader.getProperty("tripAndWayUrl"));
 
-  }
+    }
 
-  @AfterMethod
-  public void tearDown() {
-    Driver.quitDriver();
-  }
-LoginPage loginPage=new LoginPage();
-  @Test
-  public void TC019_01logInToWebPageWithRegisteredNameAndPassword() {
-    extentTest = extentReports.createTest("TC019_01", "User should be able to loginto admin page with registered name and password");
+    @AfterMethod
+    public void tearDown() {
+        Driver.quitDriver();
+    }
+
     LoginPage loginPage = new LoginPage();
-    BasePage basePage = new BasePage();
-    basePage.acceptCookiesButton.click();
-    ReusableMethods.waitFor(2);
-    loginPage.loginButtonHomePage.click();
-    loginPage.loginPageEmailAddressTextBox.sendKeys(ConfigReader.getProperty("userLoginEmailCorrect"));
-    loginPage.loginPagePasswordBox.sendKeys(ConfigReader.getProperty("userLoginPasswordCorrect"));
-    loginPage.loginPageLoginButton.click();
-    ReusableMethods.waitFor(2);
-    basePage.homePageHomeButton.click();
-    String actualUrl = Driver.getDriver().getCurrentUrl();
-    String expectedUrl = "https://qa.tripandway.com/";
-    Assert.assertEquals(actualUrl, expectedUrl);
-    extentTest.pass("User can successfuly log into admin page");
+
+    @Test
+    public void TC019_01logInToWebPageWithRegisteredNameAndPassword() {
+        extentTest = extentReports.createTest("TC019_01", "User should be able to loginto account with registered name and password");
+        LoginPage loginPage = new LoginPage();
+        BasePage basePage = new BasePage();
+        basePage.acceptCookiesButton.click();
+        ReusableMethods.waitFor(2);
+        loginPage.loginButtonHomePage.click();
+        loginPage.loginPageEmailAddressTextBox.sendKeys(ConfigReader.getProperty("userLoginEmailCorrect"));
+        loginPage.loginPagePasswordBox.sendKeys(ConfigReader.getProperty("userLoginPasswordCorrect"));
+        loginPage.loginPageLoginButton.click();
+        ReusableMethods.waitFor(2);
+        basePage.homePageHomeButton.click();
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+        String expectedUrl = "https://qa.tripandway.com/";
+        Assert.assertEquals(actualUrl, expectedUrl);
+        extentTest.pass("User can successfuly log into admin page");
 
 
-  }
+    }
 
 
 }
